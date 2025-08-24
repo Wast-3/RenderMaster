8a96e0d56f97e358e1fef8e7fb1d727b15bc2f62
wast <christianbeach@outlook.com>
2025-08-24 02:16:36 -0500

    Add ImGui key mapper

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -92,7 +92,7 @@ public class Game : GameWindow
         mainScene.camera.ProcessKeyEvents(e);
         var io = ImGui.GetIO();
 
-        ImGuiKey key = MapOpenTKKeyToImGuiKey(e.Key);
+        ImGuiKey key = ImGuiKeyMapper.MapOpenTKKeyToImGuiKey(e.Key);
         io.AddKeyEvent(key, true);
 
 
@@ -114,7 +114,7 @@ public class Game : GameWindow
     {
         var io = ImGui.GetIO();
 
-        ImGuiKey key = MapOpenTKKeyToImGuiKey(e.Key);
+        ImGuiKey key = ImGuiKeyMapper.MapOpenTKKeyToImGuiKey(e.Key);
         io.AddKeyEvent(key, false);
 
 
@@ -176,117 +176,4 @@ public class Game : GameWindow
         GL.Viewport(0, 0, framebufferWidth, framebufferHeight);
     }
 
-    private ImGuiKey MapOpenTKKeyToImGuiKey(Keys key)
-    {
-
-        return key switch
-        {
-            Keys.Tab => ImGuiKey.Tab,
-            Keys.Left => ImGuiKey.LeftArrow,
-            Keys.Right => ImGuiKey.RightArrow,
-            Keys.Up => ImGuiKey.UpArrow,
-            Keys.Down => ImGuiKey.DownArrow,
-            Keys.PageUp => ImGuiKey.PageUp,
-            Keys.PageDown => ImGuiKey.PageDown,
-            Keys.Home => ImGuiKey.Home,
-            Keys.End => ImGuiKey.End,
-            Keys.Insert => ImGuiKey.Insert,
-            Keys.Delete => ImGuiKey.Delete,
-            Keys.Backspace => ImGuiKey.Backspace,
-            Keys.Space => ImGuiKey.Space,
-            Keys.Enter => ImGuiKey.Enter,
-            Keys.Escape => ImGuiKey.Escape,
-            Keys.Apostrophe => ImGuiKey.Apostrophe,
-            Keys.Comma => ImGuiKey.Comma,
-            Keys.Minus => ImGuiKey.Minus,
-            Keys.Period => ImGuiKey.Period,
-            Keys.Slash => ImGuiKey.Slash,
-            Keys.Semicolon => ImGuiKey.Semicolon,
-            Keys.Equal => ImGuiKey.Equal,
-            Keys.LeftBracket => ImGuiKey.LeftBracket,
-            Keys.Backslash => ImGuiKey.Backslash,
-            Keys.RightBracket => ImGuiKey.RightBracket,
-            Keys.GraveAccent => ImGuiKey.GraveAccent,
-            Keys.CapsLock => ImGuiKey.CapsLock,
-            Keys.ScrollLock => ImGuiKey.ScrollLock,
-            Keys.NumLock => ImGuiKey.NumLock,
-            Keys.PrintScreen => ImGuiKey.PrintScreen,
-            Keys.Pause => ImGuiKey.Pause,
-            Keys.KeyPad0 => ImGuiKey.Keypad0,
-            Keys.KeyPad1 => ImGuiKey.Keypad1,
-            Keys.KeyPad2 => ImGuiKey.Keypad2,
-            Keys.KeyPad3 => ImGuiKey.Keypad3,
-            Keys.KeyPad4 => ImGuiKey.Keypad4,
-            Keys.KeyPad5 => ImGuiKey.Keypad5,
-            Keys.KeyPad6 => ImGuiKey.Keypad6,
-            Keys.KeyPad7 => ImGuiKey.Keypad7,
-            Keys.KeyPad8 => ImGuiKey.Keypad8,
-            Keys.KeyPad9 => ImGuiKey.Keypad9,
-            Keys.KeyPadDecimal => ImGuiKey.KeypadDecimal,
-            Keys.KeyPadDivide => ImGuiKey.KeypadDivide,
-            Keys.KeyPadMultiply => ImGuiKey.KeypadMultiply,
-            Keys.KeyPadSubtract => ImGuiKey.KeypadSubtract,
-            Keys.KeyPadAdd => ImGuiKey.KeypadAdd,
-            Keys.KeyPadEnter => ImGuiKey.KeypadEnter,
-            Keys.KeyPadEqual => ImGuiKey.KeypadEqual,
-            Keys.LeftShift => ImGuiKey.LeftShift,
-            Keys.LeftControl => ImGuiKey.LeftCtrl,
-            Keys.LeftAlt => ImGuiKey.LeftAlt,
-            Keys.LeftSuper => ImGuiKey.LeftSuper,
-            Keys.RightShift => ImGuiKey.RightShift,
-            Keys.RightControl => ImGuiKey.RightCtrl,
-            Keys.RightAlt => ImGuiKey.RightAlt,
-            Keys.RightSuper => ImGuiKey.RightSuper,
-            Keys.Menu => ImGuiKey.Menu,
-            Keys.D0 => ImGuiKey._0,
-            Keys.D1 => ImGuiKey._1,
-            Keys.D2 => ImGuiKey._2,
-            Keys.D3 => ImGuiKey._3,
-            Keys.D4 => ImGuiKey._4,
-            Keys.D5 => ImGuiKey._5,
-            Keys.D6 => ImGuiKey._6,
-            Keys.D7 => ImGuiKey._7,
-            Keys.D8 => ImGuiKey._8,
-            Keys.D9 => ImGuiKey._9,
-            Keys.A => ImGuiKey.A,
-            Keys.B => ImGuiKey.B,
-            Keys.C => ImGuiKey.C,
-            Keys.D => ImGuiKey.D,
-            Keys.E => ImGuiKey.E,
-            Keys.F => ImGuiKey.F,
-            Keys.G => ImGuiKey.G,
-            Keys.H => ImGuiKey.H,
-            Keys.I => ImGuiKey.I,
-            Keys.J => ImGuiKey.J,
-            Keys.K => ImGuiKey.K,
-            Keys.L => ImGuiKey.L,
-            Keys.M => ImGuiKey.M,
-            Keys.N => ImGuiKey.N,
-            Keys.O => ImGuiKey.O,
-            Keys.P => ImGuiKey.P,
-            Keys.Q => ImGuiKey.Q,
-            Keys.R => ImGuiKey.R,
-            Keys.S => ImGuiKey.S,
-            Keys.T => ImGuiKey.T,
-            Keys.U => ImGuiKey.U,
-            Keys.V => ImGuiKey.V,
-            Keys.W => ImGuiKey.W,
-            Keys.X => ImGuiKey.X,
-            Keys.Y => ImGuiKey.Y,
-            Keys.Z => ImGuiKey.Z,
-            Keys.F1 => ImGuiKey.F1,
-            Keys.F2 => ImGuiKey.F2,
-            Keys.F3 => ImGuiKey.F3,
-            Keys.F4 => ImGuiKey.F4,
-            Keys.F5 => ImGuiKey.F5,
-            Keys.F6 => ImGuiKey.F6,
-            Keys.F7 => ImGuiKey.F7,
-            Keys.F8 => ImGuiKey.F8,
-            Keys.F9 => ImGuiKey.F9,
-            Keys.F10 => ImGuiKey.F10,
-            Keys.F11 => ImGuiKey.F11,
-            Keys.F12 => ImGuiKey.F12,
-            _ => ImGuiKey.None
-        };
-    }
 }
