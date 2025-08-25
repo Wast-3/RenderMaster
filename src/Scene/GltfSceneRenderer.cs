1d39642cd95104a521969374cf6523e9c4c7ddb2
e <e>
2025-08-25 15:03:49 -0500

    Improved gltf model loading tester and started working on GLTF scene loader

---
+++ src/Scene/GltfSceneRenderer.cs
@@ -0,0 +1,23 @@
+﻿using RenderMaster.Engine;
+using SharpGLTF.Schema2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderMaster.src.Scene
+{
+    class GltfSceneRenderer : ISceneLoader
+    {
+        public List<Model> LoadScene(string assetPath)
+        {
+            var ext = System.IO.Path.GetExtension(assetPath).ToLowerInvariant();
+            var modelRoot = ModelRoot.Load(assetPath);
+
+            Logger.Log($"Loaded GLTF model: {assetPath} with {modelRoot.LogicalMeshes.Count} meshes, {modelRoot.LogicalMaterials.Count} materials, and {modelRoot.LogicalNodes.Count} nodes", LogLevel.Info);
+
+            return new List<Model>();
+        }
+    }
+}
---
+++ src/Scene/ISceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderMaster.src.Scene
+{
+    interface ISceneLoader
+    {
+        // Given a path to a scene asset, load each model, apply transforms, and then return a list of Models
+
+        List<Model> LoadScene(string assetPath);
+    }
+}
